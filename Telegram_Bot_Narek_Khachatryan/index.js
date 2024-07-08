eb63440d16fa25d3ee8f6a51b3b9efd2d54b51d8
Narek Khachatryan <narekkhachatryan@MacBook-Pro-Narek.local>
2024-07-08 22:16:35 +0400

    finish

--- Telegram_Bot_Narek_Khachatryan/index.js
+++ Telegram_Bot_Narek_Khachatryan/index.js
@@ -5,84 +5,78 @@ const token = '7109191302:AAEmsNSp0vMZaXvrBtWnvV0QCcQTdpCS-bs';
 const bot = new TelegramBot(token, { polling: true });
 
 const NEWS_API_KEY = 'a992ba3cf8f0400193424bd800e82811';
-const NEWS_API_URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${NEWS_API_KEY}`;
+const NEWS_API_URL = `https://newsapi.org/v2/top-headlines?apiKey=${NEWS_API_KEY}`;
 
 let userName = {}; 
 let favoriteCountries = {};
 
 const countryCodes = [
-  { code: 'ae', name: 'United Arab Emirates' },
-  { code: 'ar', name: 'Argentina' },
-  { code: 'at', name: 'Austria' },
-  { code: 'au', name: 'Australia' },
-  { code: 'be', name: 'Belgium' },
-  { code: 'bg', name: 'Bulgaria' },
-  { code: 'br', name: 'Brazil' },
-  { code: 'ca', name: 'Canada' },
-  { code: 'ch', name: 'Switzerland' },
-  { code: 'cn', name: 'China' },
-  { code: 'co', name: 'Colombia' },
-  { code: 'cu', name: 'Cuba' },
-  { code: 'cz', name: 'Czech Republic' },
-  { code: 'de', name: 'Germany' },
-  { code: 'eg', name: 'Egypt' },
-  { code: 'fr', name: 'France' },
-  { code: 'gb', name: 'United Kingdom' },
-  { code: 'gr', name: 'Greece' },
-  { code: 'hk', name: 'Hong Kong' },
-  { code: 'hu', name: 'Hungary' },
-  { code: 'id', name: 'Indonesia' },
-  { code: 'ie', name: 'Ireland' },
-  { code: 'il', name: 'Israel' },
-  { code: 'in', name: 'India' },
-  { code: 'it', name: 'Italy' },
-  { code: 'jp', name: 'Japan' },
-  { code: 'kr', name: 'Korea' },
-  { code: 'lt', name: 'Lithuania' },
-  { code: 'lv', name: 'Latvia' },
-  { code: 'ma', name: 'Morocco' },
-  { code: 'mx', name: 'Mexico' },
-  { code: 'my', name: 'Malaysia' },
-  { code: 'ng', name: 'Nigeria' },
-  { code: 'nl', name: 'Netherlands' },
-  { code: 'no', name: 'Norway' },
-  { code: 'nz', name: 'New Zealand' },
-  { code: 'ph', name: 'Philippines' },
-  { code: 'pl', name: 'Poland' },
-  { code: 'pt', name: 'Portugal' },
-  { code: 'ro', name: 'Romania' },
-  { code: 'rs', name: 'Serbia' },
-  { code: 'ru', name: 'Russia' },
-  { code: 'sa', name: 'Saudi Arabia' },
-  { code: 'se', name: 'Sweden' },
-  { code: 'sg', name: 'Singapore' },
-  { code: 'si', name: 'Slovenia' },
-  { code: 'sk', name: 'Slovakia' },
-  { code: 'th', name: 'Thailand' },
-  { code: 'tr', name: 'Turkey' },
-  { code: 'tw', name: 'Taiwan' },
-  { code: 'ua', name: 'Ukraine' },
-  { code: 'us', name: 'United States' },
-  { code: 've', name: 'Venezuela' },
-  { code: 'za', name: 'South Africa' }
+  { code: 'ae', name: 'United Arab Emirates (AE)' },
+  { code: 'ar', name: 'Argentina (AR)' },
+  { code: 'at', name: 'Austria (AT)' },
+  { code: 'au', name: 'Australia (AU)' },
+  { code: 'be', name: 'Belgium (BE)' },
+  { code: 'bg', name: 'Bulgaria (BG)' },
+  { code: 'br', name: 'Brazil (BR)' },
+  { code: 'ca', name: 'Canada (CA)' },
+  { code: 'ch', name: 'Switzerland (CH)' },
+  { code: 'cn', name: 'China (CN)' },
+  { code: 'co', name: 'Colombia (CO)' },
+  { code: 'cu', name: 'Cuba (CU)' },
+  { code: 'cz', name: 'Czech Republic (CZ)' },
+  { code: 'de', name: 'Germany (DE)' },
+  { code: 'eg', name: 'Egypt (EG)' },
+  { code: 'fr', name: 'France (FR)' },
+  { code: 'gb', name: 'United Kingdom (GB)' },
+  { code: 'gr', name: 'Greece (GR)' },
+  { code: 'hk', name: 'Hong Kong (HK)' },
+  { code: 'hu', name: 'Hungary (HU)' },
+  { code: 'id', name: 'Indonesia (ID)' },
+  { code: 'ie', name: 'Ireland (IE)' },
+  { code: 'il', name: 'Israel (IL)' },
+  { code: 'in', name: 'India (IN)' },
+  { code: 'it', name: 'Italy (IT)' },
+  { code: 'jp', name: 'Japan (JP)' },
+  { code: 'kr', name: 'Korea (KR)' },
+  { code: 'lt', name: 'Lithuania (LT)' },
+  { code: 'lv', name: 'Latvia (LV)' },
+  { code: 'ma', name: 'Morocco (MA)' },
+  { code: 'mx', name: 'Mexico (MX)' },
+  { code: 'my', name: 'Malaysia (MY)' },
+  { code: 'ng', name: 'Nigeria (NG)' },
+  { code: 'nl', name: 'Netherlands (NL)' },
+  { code: 'no', name: 'Norway (NO)' },
+  { code: 'nz', name: 'New Zealand (NZ)' },
+  { code: 'ph', name: 'Philippines (PH)' },
+  { code: 'pl', name: 'Poland (PL)' },
+  { code: 'pt', name: 'Portugal (PT)' },
+  { code: 'ro', name: 'Romania (RO)' },
+  { code: 'rs', name: 'Serbia (RS)' },
+  { code: 'ru', name: 'Russia (RU)' },
+  { code: 'sa', name: 'Saudi Arabia (SA)' },
+  { code: 'se', name: 'Sweden (SE)' },
+  { code: 'sg', name: 'Singapore (SG)' },
+  { code: 'si', name: 'Slovenia (SI)' },
+  { code: 'sk', name: 'Slovakia (SK)' },
+  { code: 'th', name: 'Thailand (TH)' },
+  { code: 'tr', name: 'Turkey (TR)' },
+  { code: 'tw', name: 'Taiwan (TW)' },
+  { code: 'ua', name: 'Ukraine (UA)' },
+  { code: 'us', name: 'United States (US)' },
+  { code: 've', name: 'Venezuela (VE)' },
+  { code: 'za', name: 'South Africa (ZA)' }
 ];
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Hello! What is your name?');
-
-  bot.on('message', (msg) => {
-    if (!userName[chatId]) {
-      userName[chatId] = msg.text;
-      bot.sendMessage(chatId, `Nice to meet you, ${userName[chatId]}!`, {
-        reply_markup: {
-          inline_keyboard: [
-            [{ text: 'News in the World', callback_data: 'world_news' }],
-            [{ text: 'News by Country', callback_data: 'news_by_country' }],
-            [{ text: 'Favorite Countries News', callback_data: 'favorite_countries_news' }],
-          ]
-        }
-      });
+  const welcomeMessage = `Welcome to the NewsBot! I can provide you with the latest news headlines.\n\nPlease select an option:`;
+  bot.sendMessage(chatId, welcomeMessage, {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: 'News in the World', callback_data: 'world_news' }],
+        [{ text: 'News by Country', callback_data: 'news_by_country' }],
+        [{ text: 'Favorite Countries News', callback_data: 'favorite_countries_news' }],
+      ]
     }
   });
 });
@@ -90,6 +84,8 @@ bot.onText(/\/start/, (msg) => {
 bot.on('callback_query', (callbackQuery) => {
   const chatId = callbackQuery.message.chat.id;
   const action = callbackQuery.data;
+
+  console.log(`Callback query received: ${action}`);
 
   if (action === 'world_news') {
     fetchAndSendNews(chatId, 'world');
@@ -132,18 +128,27 @@ function sendCountryList(chatId) {
 function fetchAndSendNews(chatId, country) {
   let url;
   if (country === 'world') {
-    url = `${NEWS_API_URL}&country=`; 
+    url = `${NEWS_API_URL}`;
   } else {
-    url = `${NEWS_API_URL}&country=${country}`; 
+    url = `${NEWS_API_URL}&country=${country}`;
   }
+
+  console.log(`Fetching news from URL: ${url}`);
 
   axios.get(url)
     .then(response => {
       const articles = response.data.articles;
-      let newsMessage = `Here are the latest news headlines for ${country === 'world' ? 'the world' : countryCodes.find(c => c.code === country).name}:\n\n`;
+      let newsMessage = `<b>Here are the latest news headlines for ${country === 'world' ? 'the world' : countryCodes.find(c => c.code === country).name}:</b>\n\n`;
 
       articles.forEach((article, index) => {
-        newsMessage += `${index + 1}. <a href="${article.url}">${article.title}</a>\n`;
+        newsMessage += `<b>${index + 1}. <a href="${article.url}">${article.title}</a></b>\n`;
+        if (article.urlToImage) {
+          newsMessage += `<a href="${article.url}"><img src="${article.urlToImage}" alt="News Image" /></a>\n`;
+        }
+        if (article.description) {
+          newsMessage += `<i>${article.description}</i>\n`;
+        }
+        newsMessage += `\n`;
       });
 
       bot.sendMessage(chatId, newsMessage, { parse_mode: 'HTML' });
@@ -153,6 +158,7 @@ function fetchAndSendNews(chatId, country) {
       bot.sendMessage(chatId, 'Sorry, I could not fetch the news at the moment.');
     });
 }
+
 
 bot.on('polling_error', (error) => {
   console.log(error);
